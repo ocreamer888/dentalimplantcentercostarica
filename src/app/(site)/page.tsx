a6df5aa56db7698fd44555a9d875a74983c4f0a9
ocreamer888 <ocreamerdrive@gmail.com>
2025-08-03 12:06:31 -0600

    Home Page Updated

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,7 +3,6 @@ import AllOnSix from "@/components/Home/AllOnSix"
 import AllOnFour from "@/components/Home/AllOnFour/Index";
 import EstimateForm from "@/components/FreeEstimate/EstimateForm/index";
 import { AcrossBorders } from "@/components/Home/AcrossBorders";
-import KeySection from "@/components/Home/KeySection";
 
 export default function Home() {
   return (
