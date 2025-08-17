326781f7cc3c25dc9abc4c72a4364a8373555f4e
ocreamer888 <ocreamerdrive@gmail.com>
2025-08-17 14:36:09 -0600

    perfomance fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,26 @@
 import './globals.css';
 import { StrictMode } from 'react';
+import { criticalCSS } from '@/lib/critical-css';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Inline critical CSS to prevent render blocking */}
+        <style dangerouslySetInnerHTML={{ __html: criticalCSS }} />
+        
+        {/* Preload critical resources */}
+        <link 
+          rel="preload" 
+          href="/images/hero/Dental Implants - Doctor Guzman.webp" 
+          as="image" 
+          type="image/webp"
+        />
+        
+        {/* Preconnect to external domains if any */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body className="antialiased">
         <StrictMode>{children}</StrictMode>
       </body>
